d528f4c07af2d9b95e1939301363d91fc46042fe
LilaGuill <lila.guillermic@gmail.com>
2020-12-05 08:57:20 +0100

    create(filters): create filters react select

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import moviesReducer from "../components/movies/moviesSlice"
+import filtersReducer from "../components/filters/filtersSlice"
 
 export default configureStore({
   reducer: {
     movies: moviesReducer,
+    filters: filtersReducer,
   },
 })
