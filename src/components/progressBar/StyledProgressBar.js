79c8a638011399d66f834ccac158d530563c36dc
LilaGuill <lila.guillermic@gmail.com>
2020-12-06 09:50:03 +0100

    update(ProgressBar): style with style theme

--- src/components/progressBar/StyledProgressBar.js
+++ src/components/progressBar/StyledProgressBar.js
@@ -1,19 +1,20 @@
 import styled from "styled-components"
 
 const StyledProgress = styled.progress`
+  margin-top: 6px;
   width: 50px;
   appearance: none;
 
   ::-webkit-progress-bar {
     height: 10px;
-    border-radius: 20px;
-    background: pink;
+    border-radius: ${({ theme }) => theme.borderRadius};
+    background: ${({ theme }) => theme.mediumGrey};
   }
 
   ::-webkit-progress-value {
     height: 10px;
-    border-radius: 20px;
-    background: yellow;
+    border-radius: ${({ theme }) => theme.borderRadius};
+    background: ${({ theme }) => theme.secondaryColor};
   }
 `
 export default StyledProgress
